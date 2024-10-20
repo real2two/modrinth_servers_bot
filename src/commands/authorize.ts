8655fa7a6e7d4e6a7e4480b965cd6289cead0ef0
real2two <26689496+real2two@users.noreply.github.com>
2024-10-19 20:22:52 -0400

    Made the response a ephemeral

--- src/commands/authorize.ts
+++ src/commands/authorize.ts
@@ -25,27 +25,30 @@ export class AuthorizeCommand extends Command {
     const modrinthPat = interaction.options.getString("pat")?.trim();
 
     if (!modrinthPat) {
-      return interaction.reply({
-        embeds: [
-          new Embed({
-            color: 0xfee75c,
-            title: "How to authorize your Modrinth account to this Discord bot",
-            description: '1. Go to https://redacted.modrinth.com/settings/pats\n2. Click "Create a PAT".',
-            image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297352975265828874/image.png" },
-          }),
-          new Embed({
-            color: 0xfee75c,
-            description: '3. Toggle "Read user data" scope.\n4. Click "Save changes".',
-            image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297353363293470791/image.png" },
-          }),
-          new Embed({
-            color: 0xfee75c,
-            description:
-              '5. Copy your personal access token.\n6. Run the </authorize:1296583363905327114> command and paste your personal access token on the "pat" argument.',
-            image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297354004950548540/image.png" },
-          }),
-        ],
-      });
+      return interaction.reply(
+        {
+          embeds: [
+            new Embed({
+              color: 0xfee75c,
+              title: "How to authorize your Modrinth account to this Discord bot",
+              description: '1. Go to https://redacted.modrinth.com/settings/pats\n2. Click "Create a PAT".',
+              image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297352975265828874/image.png" },
+            }),
+            new Embed({
+              color: 0xfee75c,
+              description: '3. Toggle "Read user data" scope.\n4. Click "Save changes".',
+              image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297353363293470791/image.png" },
+            }),
+            new Embed({
+              color: 0xfee75c,
+              description:
+                '5. Copy your personal access token.\n6. Run the </authorize:1296583363905327114> command and paste your personal access token on the "pat" argument.',
+              image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297354004950548540/image.png" },
+            }),
+          ],
+        },
+        { ephemeral: true },
+      );
     }
 
     const userRequest = await fetch(`${env.MODRINTH_API}/user`, {
