1013127781cdb98984d4eb828256086d8c2c4f49
real2two <26689496+real2two@users.noreply.github.com>
2024-10-19 20:22:04 -0400

    Added a tutorial to the /authorize command

--- src/commands/authorize.ts
+++ src/commands/authorize.ts
@@ -2,6 +2,7 @@ import env from "../env";
 import {
   ApplicationCommandOptionType,
   Command,
+  Embed,
   type APIApplicationCommandBasicOption,
   type CommandInteraction,
 } from "@buape/carbon";
@@ -16,12 +17,36 @@ export class AuthorizeCommand extends Command {
       type: ApplicationCommandOptionType.String,
       name: "pat",
       description: "Enter your personal access token with the 'Read user data' scope here",
-      required: true,
+      required: false,
     },
   ];
 
   async run(interaction: CommandInteraction) {
-    const modrinthPat = (interaction.options.getString("pat") as string).trim();
+    const modrinthPat = interaction.options.getString("pat")?.trim();
+
+    if (!modrinthPat) {
+      return interaction.reply({
+        embeds: [
+          new Embed({
+            color: 0xfee75c,
+            title: "How to authorize your Modrinth account to this Discord bot",
+            description: '1. Go to https://redacted.modrinth.com/settings/pats\n2. Click "Create a PAT".',
+            image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297352975265828874/image.png" },
+          }),
+          new Embed({
+            color: 0xfee75c,
+            description: '3. Toggle "Read user data" scope.\n4. Click "Save changes".',
+            image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297353363293470791/image.png" },
+          }),
+          new Embed({
+            color: 0xfee75c,
+            description:
+              '5. Copy your personal access token.\n6. Run the </authorize:1296583363905327114> command and paste your personal access token on the "pat" argument.',
+            image: { url: "https://media.discordapp.net/attachments/1297352938250965023/1297354004950548540/image.png" },
+          }),
+        ],
+      });
+    }
 
     const userRequest = await fetch(`${env.MODRINTH_API}/user`, {
       headers: { authorization: modrinthPat },
