955a8f9d5128914a7560b0cd84a08b0c2db64d94
real2two <26689496+real2two@users.noreply.github.com>
2024-10-21 23:54:35 -0400

    Update getServer.ts

--- src/lib/getServer.ts
+++ src/lib/getServer.ts
@@ -16,6 +16,8 @@ export async function getServerUser(
     where: and(eq(schema.share.accessUserId, BigInt(userId)), eq(schema.share.serverId, serverId)),
   });
 
+  console.log(userId, shares);
+
   for (const share of shares) {
     const shareModrinthAuth = await getModrinthPat({ userId: share.userId.toString() });
     const shareServer = await getServerFetch(shareModrinthAuth, serverId);
